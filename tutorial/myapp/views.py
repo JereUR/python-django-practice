39cdc58b8c324e3acecb7974eb77c1071d0ed27e
JereUR <jeremias.jdv@gmail.com>
2023-12-04 20:41:01 -0300

    feat(Root): static and arsha views added

--- tutorial/myapp/views.py
+++ tutorial/myapp/views.py
@@ -9,3 +9,11 @@ def counter(request):
     text = request.POST['text']
     amount_of_words = len(text.split())
     return render(request, 'counter.html', {'amount': amount_of_words})
+
+
+def static(request):
+    return render(request, 'static.html')
+
+
+def arsha(request):
+    return render(request, 'arsha.html')
