801336fb206a9542004e6d417f868a1022a6e4b2
JereUR <jeremias.jdv@gmail.com>
2023-12-04 20:06:52 -0300

    feat(Root): Count number of words sended from index view

--- tutorial/myapp/views.py
+++ tutorial/myapp/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {
-        'data': 'Data Send',
-        'text': 'Data Recieved'
-    }
+    return render(request, 'index.html')
 
-    return render(request, 'index.html', context)
+
+def counter(request):
+    text = request.POST['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount': amount_of_words})
