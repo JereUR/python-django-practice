a2adc647bc6b4f9845d712a0439793975cac8dfd
JereUR <jeremias.jdv@gmail.com>
2023-12-05 19:37:40 -0300

    feat(Root): Change path for arsha, now is the index, and add form-counter, register, login and logout paths

--- tutorial/myapp/urls.py
+++ tutorial/myapp/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('counter', views.counter, name='counter'),
-    path('static', views.static),
-    path('arsha', views.arsha)
+    path('static', views.static, name='static'),
+    path('form-counter', views.formCounter, name='formCounter'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 ]
