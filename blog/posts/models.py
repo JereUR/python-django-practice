a9b145deb31e4618225d34efd4ba2d422bd2bd51
JereUR <jeremias.jdv@gmail.com>
2023-12-07 22:03:41 -0300

    feat(Root): Post model created

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000000)
+    created_at = models.DateTimeField(auto_now_add=True)
