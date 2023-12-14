b7fa75c4dcbe0c0d873124d883d9a31c935cc09d
JereUR <jeremias.jdv@gmail.com>
2023-12-13 21:39:38 -0300

    feat(Root): Post method created

--- drfproject/drfproject/views.py
+++ drfproject/drfproject/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from drfapp.serializers import StudentSerializer
+from drfapp.models import Student
+
 
 class TesView(APIView):
     def get(self, request, *args, **kwargs):
@@ -10,3 +13,11 @@ class TesView(APIView):
             'years_active': 10
         }
         return Response(data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
